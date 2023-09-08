8c322955ad1a94ec3d5c9bcc64d8537c53942254
Charlie Greenman <charlie@razroo.com>
2023-09-08 14:30:40 -0400

    Add code for angular-16.2.0 create-workspace

---
+++ apps/test-org-workspace-gen/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-org-workspace-gen-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-org-workspace-gen';
+}
